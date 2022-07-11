dfdd1069b1e5d3b2ac04ce04f242085d4f54c663
Apheayenne <Zelda.ganondorf@gmail.com>
2022-07-11 10:18:35 -0500

    Edit unnecessary files

--- TwilightPrincessTracker/subWindows.js
+++
@@ -1,9 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-function Setting(element) {
-    //element.getElementById("Rando-Btn").style.color = "#ffffff";
-    element.style.color = "#ffffff";
-}
